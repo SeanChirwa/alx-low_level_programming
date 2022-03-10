534a637c62bcd9b01fea4fcfe199dc67e3dd6e50
SeanChirwa <benjaminchirwa17@gmail.com>
2022-03-10 14:45:14 -0800

    task 8 fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,6 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
-	write(1, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-=======
-	fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
->>>>>>> f5cdc0f6dae3d841fdf51c611be1f38ac8b7acbb
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
